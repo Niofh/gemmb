7ef67e6d5742d1e4c5a2cdcb752d38f4fda2798f
Nicofh <598220654@qq.com>
2019-08-08 01:02:55 +0800

    add 头部组件

---
+++ src/components/table-row/index.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="table-row">
+    <div class="my-table">
+      <div class="row-wrap">
+
+        <div class="item-col"
+             v-for="(item,i) in rows"
+             :key="i"
+             :style="{'width':item.width}"
+        >
+          <div class="header-item" :data-index="index" @click="onSelect(i,$event)">
+            <div class="header-col">
+              <span class="name">{{item.name}}</span>
+              <van-icon v-if="item.selectOptions&&item.selectOptions.length>0" custom-class="icon-arrow"
+                        :name="i===index&&selectShow?'arrow-up':'arrow-down'"
+                        color="#fff"></van-icon>
+            </div>
+          </div>
+          <div v-show="item.selectOptions&&item.selectOptions.length&&i===index?selectShow:false" class="my-select">
+            <div class="up-arrow"></div>
+            <ul class="wrap">
+              <li class="item" v-for="select in item.selectOptions" :key="select.id">{{select.name}}</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "table-row",
+    props: {
+      rows: Array
+    },
+    data() {
+      return {
+        selectShow: false, //控制下拉列表的显示隐藏，false隐藏、true显示
+        index: -1 //选择的下拉列表下标
+      }
+    },
+    methods: {
+      onSelect(index, e) {
+        console.log(e)
+        const oldIndex = e.mp.currentTarget.dataset.index
+
+        if (oldIndex === index) {
+
+          this.selectShow = !this.selectShow
+        } else {
+          this.selectShow = true
+        }
+        this.index = index
+      },
+      arrow() {
+        return "arrow-down"
+      }
+    }
+  }
+</script>
+
+
+<style lang="stylus" scoped>
+  @import "~@/assets/stylus/common.styl"
+  .my-table {
+    padding 0 rpx(30)
+
+    .row-wrap {
+      position relative
+      border-radius rpx(8)
+      background-color #2E2E2D
+
+      .item-col {
+        position relative
+        z-index 9
+        font-size rpx(28)
+        color #fff
+        text-align center
+        display inline-block
+        box-sizing border-box
+        width 25%
+
+        &:last-child {
+          .header-col {
+            border-right none
+          }
+
+          .my-select {
+            right: 0
+          }
+        }
+      }
+
+      .header-item {
+        padding rpx(20) 0px
+      }
+
+      .header-col {
+        border-right rpx(3) solid #fff
+
+        >>> .icon-arrow {
+          vertical-align middle
+          margin-left rpx(8)
+        }
+
+        .name {
+          line-height rpx(24)
+        }
+      }
+
+      .my-select {
+        position absolute
+        left: 0
+        top rpx(80)
+        background #000
+      }
+
+      .wrap {
+        padding rpx(5)
+
+        .item {
+          padding rpx(18) rpx(52) rpx(18) rpx(23)
+          text-align left
+          font-size: rpx(24);
+          font-weight: 400;
+          color: #FEFEFE;
+          border-bottom: rpx(3) solid #DFDFDF;
+
+          &:last-child {
+            border-bottom none
+          }
+        }
+
+      }
+
+      .up-arrow {
+        position absolute
+        left 15%
+        top rpx(-36)
+        width: 0;
+        height: 0;
+        border-top: rpx(20) solid rgba(0, 0, 0, 0);
+        border-right: rpx(16) solid rgba(0, 0, 0, 0);
+        border-bottom: rpx(20) solid #000;
+        border-left: rpx(16) solid rgba(0, 0, 0, 0);
+        z-index 10
+      }
+    }
+  }
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,25 +5,8 @@
       <div class="process-2"></div>
     </div>
 
-    <div class="box"></div>
 
-    <div class="my-table">
-      <div class="row">
-        <div class="header-col">
-          <span class="name">优先级</span> <van-icon custom-class="icon-arrow"  name="arrow-down" color="#fff" />
-        </div>
-        <div class="header-col">
-          <span class="name">优先级</span> <van-icon custom-class="icon-arrow"  name="arrow-down" color="#fff" />
-        </div>
-        <div class="header-col">
-          <span class="name">优先级</span> <van-icon custom-class="icon-arrow"  name="arrow-down" color="#fff" />
-        </div>
-        <div class="header-col">
-          <span class="name">优先级</span> <van-icon custom-class="icon-arrow"  name="arrow-down" color="#fff" />
-        </div>
-      </div>
-    </div>
-
+    <table-row :rows="rows"></table-row>
 
     <my-footer :active="0"></my-footer>
   </div>
@@ -31,54 +14,94 @@
 
 <script>
   import myFooter from "@/components/my-footer/index.vue";
+  import tableRow from "@/components/table-row/index.vue";
 
   export default {
     components: {
-      "my-footer": myFooter
+      "my-footer": myFooter,
+      "table-row": tableRow,
     },
     data() {
-      return {};
+      return {
+        check: false,
+        rows: [
+          {
+            width: '22%',
+            name: "优先级",
+            selectOptions: [
+              {
+                id: 1,
+                name: "Critical"
+              },
+              {
+                id: 2,
+                name: "Major"
+              },
+              {
+                id: 3,
+                name: "Minor"
+              },
+              {
+                id: 4,
+                name: "Warining"
+              },
+              {
+                id: 5,
+                name: "Normal"
+              },
+
+            ]
+          },
+          {
+            width: '28%',
+            name: "设备名称"
+          },
+          {
+            width: '28%',
+            name: "设备IP"
+          },
+          {
+            width: '22%',
+            name: "地点"
+          }
+        ]
+      };
+    },
+    methods: {
+      onChangeSelect(e) {
+        console.log(e)
+        this.check = !this.check
+
+      },
+      onHome(e) {
+        console.log(e, "home")
+      },
+      onHide() {
+        this.check = false
+      }
     }
 
   };
 </script>
 
+
 <style lang="stylus" scoped>
   @import "~@/assets/stylus/common.styl"
-  .my-table{
-    .row{
-      overflow: hidden
-      border-radius rpx(8)
-      .header-col{
-        width 20%
-        padding rpx(20) 0px
-        float left
-        text-align center
-        font-size rpx(28)
-        color #fff
-        background-color #2C2C2C
-        >>>.icon-arrow{
-          vertical-align middle
-          margin-left rpx(5)
-        }
-        .name{
-          line-height rpx(24)
-        }
-      }
-    }
-  }
+
   .process {
     width 80%
     height: rpx(16);
     background: rgba(12, 200, 8, 1);
     border-radius: 8px;
     overflow hidden
+
     .process-1 {
       width 10%
       float left
       background-color red
       height: rpx(16)
     }
+
     .process-2 {
       float left
       width 20%
@@ -87,13 +110,5 @@
     }
   }
 
-  .box {
-    width: 0;
-    height: 0;
-    border-top: rpx(20) solid rgba(0, 0, 0, 0);
-    border-right: rpx(16) solid rgba(0, 0, 0, 0);
-    border-bottom: rpx(20) solid #474544;
-    border-left: rpx(16) solid rgba(0, 0, 0, 0);
-  }
 
 </style>
